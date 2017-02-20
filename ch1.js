d0cbf2c210c7ae214613d8df220aba431ddc7137
myson <hemostatic@gmail.com>
2017-02-20 10:08:36 -0500

    Add functions for printing, calculating total

--- ch1.js
+++ ch1.js
@@ -5,3 +5,19 @@ const SPENDING_THRESHOLD = 150;
 
 var bank_balance = 302.13;
 
+function printAmount(amt) {
+    console.log( "$" + amt.toFixed( 2 ) );
+}
+
+function calculateFinalPurchaseAmount(amt) {
+    // calculate the new amount with the tax
+    amt = amt + (amt * TAX_RATE);
+
+    // return the new amount
+    return amt;
+}
+
+function purchasePhone() {
+	// purchase phones and accessories until out of money
+	// TODO
+}
